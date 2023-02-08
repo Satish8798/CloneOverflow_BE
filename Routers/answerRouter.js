c2e4c43876148b1d24d6a4cfb9d6c284f085ce4b
Satish Ganesan <ganesansatishm7@gmail.com>
2023-02-09 01:09:43 +0530

    rectified mistakes

--- Routers/answerRouter.js
+++ Routers/answerRouter.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.post("/create-answer",AuthModule.authenticateUser,answerModule.createAnswer);
 router.post("/get",answerModule.getAnswers);
-router.post("/up-vote",AuthModule.authenticateUser,answerModule.upVote);
-router.post("/down-vote",AuthModule.authenticateUser,answerModule.downVote);
+router.put("/up-vote",AuthModule.authenticateUser,answerModule.upVote);
+router.put("/down-vote",AuthModule.authenticateUser,answerModule.downVote);
 
 module.exports= router
--- Routers/questionsRouter.js
+++ Routers/questionsRouter.js
@@ -6,8 +6,8 @@ const router= express.Router();
 
 router.post("/create-question",AuthModule.authenticateUser,questionModule.createQuestion);
 router.get("/get",questionModule.getQuestions);
-router.post("/upvote",AuthModule.authenticateUser,questionModule.upVote);
-router.post("/downvote",AuthModule.authenticateUser,questionModule.downVote);
+router.put("/upvote",AuthModule.authenticateUser,questionModule.upVote);
+router.put("/downvote",AuthModule.authenticateUser,questionModule.downVote);
 router.get("/get-results",questionModule.getResults);
 router.post("/:questionId",questionModule.getQuestionDetails);
 router.delete("/delete/:questionId",questionModule.deleteQuestion);
